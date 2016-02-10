771254b4051dc2aa38959a7ccf7bc36f934a5eb3
Patrick Ward <JPatrickWard@users.noreply.github.com>
2016-02-10 14:54:07 -0700

    Update find_largest_value.rb

--- find_largest_value.rb
+++ find_largest_value.rb
@@ -1,31 +1,24 @@
 # This program will have a method that will take an array of numbers as input parameter. Then find the largest and return it.
 
 numbers = Array.new
-numbers = [1,234,23,245,34,423,534,45,4,234,2345,3246,245,6435,76,456,675]
+numbers = [0,-1,234,23,245,34,240,676,534,765,4,56,7,65,4,3,34,34,34,-1,45,4,234,276,456,675,-100]
+# numbers = []
+# array_length = numbers.length
 
 def find_max_num(numbers)
-  # Check for 0 length array. If zero length then return 
-  if numbers.length > 0
-	for i in 0..numbers.length 
-	  if numbers[i] > numbers[i + 1]
-	    max_num = numbers[i]
-	  	i += 1
-	  	# break
-	  else max_num = numbers[i + 1]
-	  	i += 1
-	  	# break
-	  end 
-	  puts "max_num is #{max_num}."
-	  	# if i == array_length
-	  	# 	break
-	  	# 	else i += 1
-	  	# end
-    end
+  if numbers.length == 0 
+  	puts "No numbers were passed." 
+  elsif numbers.length > 0
+    max_num = numbers[0]
+  	for i in 1..numbers.length - 1 # So I don't over-run the array, and start at the 2nd position to compare against max at postion [0]
+      if max_num < numbers[i] 
+        max_num = numbers[i] 
+      end
+      i += 1
+  	end
+    puts "Looked through an array of lenght #{numbers.length}."
+    puts "The largest value is #{max_num}"
   end
-	  puts "The largest number in that set is #{max_num}."
 end
 
 find_max_num(numbers)
-
-
-	
